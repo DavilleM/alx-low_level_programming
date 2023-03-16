d74ba423a3f4137c58b3cbc2deac8942d1c7868a
David Mwangi <lillkym68@gmail.com>
2023-03-16 11:50:34 -0700

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the main function and calls all other functions
+ * Return: always zero
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
