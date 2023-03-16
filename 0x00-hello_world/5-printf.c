0e66d5866f53f30ca7ee7033893844b5e6624df0
David Mwangi <lillkym68@gmail.com>
2023-03-16 11:59:27 -0700

    second program using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - is the main functionand calls other functions
+ *
+ * Return: always return zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
